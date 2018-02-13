7c57f15e495af345695a35ed8430211cce309d91
Brandon Slade <brandonyoyoslade@gmail.com>
2018-02-13 12:17:51 -0700

    api-server: expose websocket to all origins; log socket lifecycle events

--- api-server/src/game/game-service.ts
+++ api-server/src/game/game-service.ts
@@ -6,8 +6,14 @@ export class GameService {
     ) { }
     
     async start() {
+        console.log(`Game version: ${GameVersion}`);
+        
         this.io.on('connection', socket => {
+            console.log(`Client connected: ${socket.id}`);
             socket.emit('game-version', GameVersion);
+            socket.on('disconnect', () => {
+                console.log(`Client disconnected: ${socket.id}`);
+            });
         });
         
         setInterval(() => {
--- api-server/src/sockets.ts
+++ api-server/src/sockets.ts
@@ -4,7 +4,9 @@ import * as socketIO from 'socket.io';
 export let io: SocketIO.Server;
 export function initializeSocketServer(server: Server): Promise<SocketIO.Server> {
     return new Promise((resolve, reject) => {
-        let socketServer = io = socketIO(server);
+        let socketServer = io = socketIO(server, {
+            origins: '*:*'
+        });
         resolve(socketServer);
     });
 }
