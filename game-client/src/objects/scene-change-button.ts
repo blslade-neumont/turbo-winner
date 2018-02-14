b97d2a78c6adc283be2748bf353cce47f68c1f59
David <dduran@student.neumont.edu>
2018-02-14 16:01:22 -0700

    Objects: created (untested) start button

---
+++ game-client/src/objects/scene-change-button.ts
@@ -0,0 +1,50 @@
+import {GameObject, GraphicsAdapter, DefaultGraphicsAdapter, GameEvent, MouseButton} from "engine";
+import { PlayScene } from "..";
+export class ChangeButton extends GameObject{
+
+    private hover : boolean;
+    private color : string;
+    private _h : number;
+    private _w : number;
+
+    constructor(position : Int8Array, height : number, width : number){
+        super("ChangeButton");
+        this.x = position[0];
+        this.y = position[1];
+        this._h = height;
+        this._w = width;
+    }
+
+    renderImpl(adapter : GraphicsAdapter){
+        if(adapter instanceof DefaultGraphicsAdapter){
+            let context = adapter.context!;
+            context.fillStyle = this.color;
+            context.fillRect(this.x, this.y, this._w, this._h)
+            context.lineWidth = 2;
+            context.strokeStyle = "#130000"
+        }
+    }
+
+    handleEvent(event : GameEvent){
+        if (this.hover && event.type == "mouseButtonPressed" && event.button == MouseButton.Left){
+            //start the game/go to main game scene
+            this.game.changeScene(new PlayScene());
+            return true;
+        }
+        return super.handleEvent(event);
+    }
+
+    tick(delta : number){
+        let  mousePosWorld = this.scene.camera!.transformPixelCoordinates(this.game.eventQueue.mousePosition);
+
+        if(mousePosWorld[0] >= this.x && mousePosWorld[0] <= this._w  &&
+             mousePosWorld[1] >= this.y && mousePosWorld[1] <= this._h){
+                this.hover = true;
+        }
+        if(this.hover){
+            this.color = "#FF0D1A"
+        }
+        else this.color = "#872216";
+    }
+    
+}
