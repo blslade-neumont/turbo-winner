9ec13bc9a48acf34f372bb29427ed78e14337045
Brandon Slade <brandonyoyoslade@gmail.com>
2018-03-02 10:41:59 -0700

    frontend: add autosave observable on a debounce

--- frontend/src/modules/+account/pages/account/account.ts
+++ frontend/src/modules/+account/pages/account/account.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { tap, switchMap, debounceTime } from 'rxjs/operators';
 import { ComponentBase } from 'utils/components';
 import { AuthService } from 'services';
 import { User } from 'models';
 import { colors } from 'dbs';
+
+const AUTOSAVE_DEBOUNCE_MILLIS = 600;
 
 @Component({
     templateUrl: './account.html',
@@ -27,8 +31,10 @@ export class AccountComponent extends ComponentBase {
     }
     set color(val: string | null) {
         if (!val) return;
+        if (this.color === val) return;
         this._colorOverride = val;
         if (this.currentUser && this.currentUser.color === this._colorOverride) delete this._colorOverride;
+        this._autosaveSubject.next(void(0));
     }
     
     private _nicknameOverride: string | undefined;
@@ -38,15 +44,41 @@ export class AccountComponent extends ComponentBase {
     }
     set nickname(val: string | null) {
         if (!val) return;
+        if (this.nickname === val) return;
         this._nicknameOverride = val;
         if (this.currentUser && this.currentUser.nickname === this._nicknameOverride) delete this._nicknameOverride;
+        this._autosaveSubject.next(void(0));
     }
+    
+    private _autosaveSubject = new Subject<void>();
+    isDirty = false;
+    isSaving = false;
+    wasSaved = false;
     
     ngOnInit() {
         super.ngOnInit();
+        
         this.subscriptions.push(this.auth.currentUserObservable.subscribe(cuser => {
             this.currentUser = cuser;
         }));
+        
+        let autosaveObservable = this._autosaveSubject.pipe(
+            tap(() => (this.isDirty = true, this.isSaving = false)),
+            debounceTime(AUTOSAVE_DEBOUNCE_MILLIS),
+            switchMap(() => {
+                this.isDirty = false;
+                this.isSaving = true;
+                return this.save();
+            }),
+            tap(() => (this.isSaving = false, this.wasSaved = true))
+        );
+        this.subscriptions.push(autosaveObservable.subscribe());
+    }
+    
+    save() {
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve, 500);
+        });
     }
     
     logIn() {
